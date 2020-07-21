37e261f31345c6779dec4341d7bb1144bd832dde
Juri F <friindel@mail.ru>
2020-07-21 12:15:30 +0100

    updated notification messages

--- Gemfile
+++ Gemfile
@@ -24,6 +24,7 @@ gem 'jbuilder', '~> 2.7'
 # Use Active Model has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 gem 'devise'
+gem 'toastr-rails'
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
 
@@ -33,6 +34,7 @@ gem 'bootsnap', '>= 1.4.2', require: false
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'spring'
 end
 
 group :development do
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:fullname])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:fullname])
+  end
+
+  def confirm_logged_in
+   #don't need to "return" anything here
+   redirect_to new_sessions_path, notice: "Please login" unless session[:user_id]
+  end
+
 end
